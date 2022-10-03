2cd5075d2867d8bddbf105d5a34d2400c9de1fb4
taekhun <brachiosaurus19@gmail.com>
2022-10-03 20:13:26 +0900

    캐릭터 스프라이트 확인

--- turtle_test.py
+++ turtle_test.py
@@ -1,17 +1,16 @@
 from pico2d import *
 open_canvas()
-character = load_image('turtle_1.png')
+character = load_image('idle_character.png')
 
 
 frame = 0
 for x in range(0, 800, 5):
     clear_canvas()
-    character.clip_draw(frame * 50, 0, 50, 148, x, 74)
+    character.clip_draw(frame * 32, 0, 32, 32, x, 32)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
     get_events()
-
 
 close_canvas()
 
