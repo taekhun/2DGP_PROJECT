436f63c785957b6f11d0631c299a7edec3abf73c
taekhun <brachiosaurus19@gmail.com>
2022-10-03 20:11:04 +0900

    거북이 스프라이트 확인

--- turtle_test.py
+++ turtle_test.py
@@ -1,14 +1,14 @@
 from pico2d import *
 open_canvas()
-character = load_image('turtle_attack.png')
+character = load_image('turtle_1.png')
 
 
 frame = 0
 for x in range(0, 800, 5):
     clear_canvas()
-    character.clip_draw(frame * 100, 0, 100, 100, x, 60)
+    character.clip_draw(frame * 50, 0, 50, 148, x, 74)
     update_canvas()
-    frame = (frame + 1) % 3
+    frame = (frame + 1) % 8
     delay(0.05)
     get_events()
 
