21d75eb19d53eb627f950cb6afc897b4e68b0c5a
taekhun <brachiosaurus19@gmail.com>
2022-10-03 20:17:13 +0900

    sprite크기 최종 조절

--- turtle_test.py
+++ turtle_test.py
@@ -1,12 +1,12 @@
 from pico2d import *
 open_canvas()
-character = load_image('idle_character.png')
+character = load_image('turtle_1.png')
 
 
 frame = 0
 for x in range(0, 800, 5):
     clear_canvas()
-    character.clip_draw(frame * 32, 0, 32, 32, x, 32)
+    character.clip_draw(frame * 32, 0, 32, 96, x, 48)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
