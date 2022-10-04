46990258a0040c7cbcf2e6651b235425620e07df
taekhun <brachiosaurus19@gmail.com>
2022-10-05 01:39:37 +0900

    적군 사망, 공격 확인

--- turtle_test.py
+++ turtle_test.py
@@ -1,14 +1,14 @@
 from pico2d import *
 open_canvas()
-character = load_image('turtle_1.png')
+character = load_image('special_attack.png')
 
 
 frame = 0
 for x in range(0, 800, 5):
     clear_canvas()
-    character.clip_draw(frame * 32, 0, 32, 96, x, 48)
+    character.clip_draw(frame * 16, 0, 16, 15, 30, 31)
     update_canvas()
-    frame = (frame + 1) % 8
+    frame = (frame + 1) % 1
     delay(0.05)
     get_events()
 
