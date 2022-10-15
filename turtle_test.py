7c01fe696571ae4f88886d20efce3b10627f0062
taekhun <brachiosaurus19@gmail.com>
2022-10-16 02:31:24 +0900

    캐릭터 사망 sprite 확인

--- turtle_test.py
+++ turtle_test.py
@@ -1,12 +1,12 @@
 from pico2d import *
 open_canvas()
-character = load_image('special_attack.png')
+character = load_image('grave.png')
 
 
 frame = 0
 for x in range(0, 800, 5):
     clear_canvas()
-    character.clip_draw(frame * 16, 0, 16, 15, 30, 31)
+    character.clip_draw(0, 0, 32, 32, 400, 50)
     update_canvas()
     frame = (frame + 1) % 1
     delay(0.05)
