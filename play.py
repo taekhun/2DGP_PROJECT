cdaaa012604f7204a083a2f5ed1c43422ff066bd
taekhun <brachiosaurus19@gmail.com>
2022-10-17 23:46:10 +0900

    맵 색상 변경

--- play.py
+++ play.py
@@ -1,15 +1,16 @@
 from pico2d import *
 import character
 open_canvas()
+stage_1 = load_image('stage1.png')
 ch = character.Character()
 
 while ch.running:
     ch.handle_events()
     ch.update()
     clear_canvas()
+    stage_1.draw(400, 300)
     ch.draw()
     update_canvas()
     delay(0.02)
 
 close_canvas()
-
