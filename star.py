fac7e92d28956d26a0bb37c7c6fd6cda062514a6
taekhun <brachiosaurus19@gmail.com>
2022-11-07 13:36:52 +0900

    별의 y축 범위 추가

--- star.py
+++ star.py
@@ -18,7 +18,7 @@ class Star:
             self.x += self.velocity
         elif self.UD_direction == 1 or self.UD_direction == -1:
             self.y += self.velocity
-        if self.x < 20 or self.x > 800 - 20:
+        if self.x < 20 or self.x > 800 - 20 or self.y < 20 or self.y > 600 - 20:
             game_world.remove_object(self)
 
     def get_direction(self, cur_RL_dir, cur_UD_dir):
