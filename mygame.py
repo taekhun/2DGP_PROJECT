80e7647bbb8b1c1db7495f43ad324316b3bfa29a
taekhun <brachiosaurus19@gmail.com>
2022-11-07 13:36:27 +0900

    타이틀 상태 추가

--- mygame.py
+++ mygame.py
@@ -1,8 +1,8 @@
 import game_framework
 import pico2d
 
-import play_state_1
+import title_state
 
 pico2d.open_canvas()
-game_framework.run(play_state_1)
+game_framework.run(title_state)
 pico2d.close_canvas()
