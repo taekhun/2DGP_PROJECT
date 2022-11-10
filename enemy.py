a8beb3e419ad4f5c3de8e31ae507e8e909e3721b
taekhun <brachiosaurus19@gmail.com>
2022-11-11 01:34:27 +0900

    적군 데모버전 추가

---
+++ enemy.py
@@ -0,0 +1,34 @@
+from pico2d import *
+import random
+import game_framework
+import game_world
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0) / 60.0
+RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+class Enemy:
+    image = None
+
+    def __init__(self):
+        if Enemy.image == None:
+            Enemy.image = load_image('turtle_1.png')
+        self.frame = random.randint(0, 7)
+        self.x = random.randint(380, 430)
+        self.y = 550
+        self.move_speed = 0.5
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        self.y -= self.move_speed * RUN_SPEED_PPS * game_framework.frame_time
+        if self.y < 300 or self.y > 600 - 20:
+            game_world.remove_object(self)
+
+    def draw(self):
+        self.image.clip_draw(int(self.frame) * 32, 32 * 3, 32, 32, self.x, self.y)
